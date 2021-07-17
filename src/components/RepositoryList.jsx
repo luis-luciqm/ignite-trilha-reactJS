67f83d9dbc6160171767c2bc42339a94fda1df2c
luis-luciqm <redbullfc18@gmail.com>
2021-07-16 23:15:22 -0300

    Iniciando estrutura do componente RepositoryList no ReactJS

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in ReactJS</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import { render } from 'react-dom';
 import { App } from './App'
+// import { RepositoryList } from './components/RepositoryList'
 
 render(<App />, document.getElementById('root'))
