b21c6873b74231a04ae2b97f7e18feff21abe885
luis-luciqm <redbullfc18@gmail.com>
2021-07-16 21:21:54 -0300

    Corrigindo erros de configuração css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './styles/global.css'
 
 export function App() {
-    throw new Error('Eita carai, swing da zebra')
-
     return <h1>Hello, ReactJS</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
                 use: 'babel-loader',
             },
             {
-                test: /\.jsx$/,
+                test: /\.css$/,
                 exclude: /node_modules/,
                 use: ['style-loader', 'css-loader'],
             }
