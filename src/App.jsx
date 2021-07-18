afb1e17588f2e1ad141d8acde91e67ca31b61846
luis-luciqm <redbullfc18@gmail.com>
2021-07-17 22:11:11 -0300

    Estado do componente ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,10 @@ import './styles/global.scss'
 import { RepositoryList } from './components/RepositoryList'
 
 export function App() {
-    return <RepositoryList />
+    return (
+        <div>
+            <RepositoryList />
+            <Counter />
+        </div>
+    );
 }
