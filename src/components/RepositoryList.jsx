9ed3fc1a6ac3792b06f64a39608974091659202e
luis-luciqm <redbullfc18@gmail.com>
2021-07-22 00:42:10 -0300

    Mostrando dados da API na tela usando map

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { useEffect, useState } from 'react';
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: "Unform",
-    description: "Array Index Out Of Bounds",
-    link: "https://github.com/"
-}
-
-// https://api.github.com/orgs/rocketseat/repos
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -25,9 +17,10 @@ export function RepositoryList() {
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository = {repository}/>
-                <RepositoryItem repository = {repository}/>
-                <RepositoryItem repository = {repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository = {repository}/> 
+                })}
+                
             </ul>
         </section>
     );
